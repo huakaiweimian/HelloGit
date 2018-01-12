817cc087bf7c83be79031515203928214aaf0e66
huakaiweimian <544528359@qq.com>
2018-01-12 19:42:13 +0800

    这是TM谁写的破玩意啊，报错的啊，知不知道啊

--- src/main/java/com/huakai/git/HelloGit.java
+++ src/main/java/com/huakai/git/HelloGit.java
@@ -1,5 +1,5 @@
 package com.huakai.git;
 
 public class HelloGit {
-
+	
 }
--- src/test/java/com/huakai/test/GitTest.java
+++ src/test/java/com/huakai/test/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
 		System.out.println("你好s");
 		System.out.println("aafafaf afasfa");
 		System.out.println("写的什么鬼东西");
-                system.out.println("都闪开，让我来写");
+        System.out.println("都闪开，让我来写");
 	}
 }
